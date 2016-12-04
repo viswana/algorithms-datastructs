69d489e9e9c10e6fe93dce5e5392f76d70f7fc33
Viswanathan <vsscrum@gmail.com>
2016-12-03 23:08:40 -0800

    Added recursive implementation to reverseWords.c

--- SatMeetups/Dec032016/reverseWords.c
+++ SatMeetups/Dec032016/reverseWords.c
@@ -7,8 +7,8 @@
 #include <string.h>
 
 void reverseWords(char* a) {
-  if (a == NULL || strlen(a) < 2) return;
-  size_t n = strlen(a);
+  size_t n;
+  if (a == NULL || (n = strlen(a)) < 2) return;
   for (size_t i = 0; i < n/2; i++) {
     char tmp = a[i];
     a[i] = a[n-i-1];
@@ -28,10 +28,35 @@ void reverseWords(char* a) {
   printf("%s\n", a);
 }
 
+void reverseWordR(char* a, size_t n) {
+  if (a == NULL || n < 2) return;
+  char tmp = a[0];
+  a[0] = a[n-1];
+  a[n-1] = tmp;
+  reverseWordR(a+1, n-2);
+}
+
+void reverseWordsR(char* a) {
+  size_t n;
+  if (a == NULL || (n = strlen(a)) < 2) return;
+  reverseWordR(a, n);
+  size_t i = 0, j = 0;
+  while (a[i] != '\0') {
+    while (a[j] != ' ' && a[j] != '\0') j++;
+    reverseWordR(a+i, j-i);
+    if (a[j] == ' ') j++;
+    i = j;
+  }
+  printf("%s\n", a);
+}
+
 int main() {
   char a[] = "a good day";
   reverseWords(a);
   char b[] = "todays meetup was quite interesting";
   reverseWords(b);
+  reverseWordsR(a);
+  reverseWordsR(b);
+
   return 0;
 }
